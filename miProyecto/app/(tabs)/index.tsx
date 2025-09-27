d5236bda3fdf8d3a35f9a9c1915a93392b38a3be
Katleen Levis <113138472+mishellyla@users.noreply.github.com>
2025-09-27 06:16:15 +0000

    Probandolo parte 1

--- miProyecto/app/(tabs)/index.tsx
+++ miProyecto/app/(tabs)/index.tsx
@@ -7,6 +7,7 @@ import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
+
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
@@ -18,7 +19,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Aqui estamos pronbando!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -59,6 +60,7 @@ export default function HomeScreen() {
             </Link.Menu>
           </Link.Menu>
         </Link>
+        
 
         <ThemedText>
           {`Tap the Explore tab to learn more about what's included in this starter app.`}
